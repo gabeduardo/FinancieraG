f761ade0b774b48a656b03181e289e29b0ccc81f
gabeduardo <gemcir13@gmail.com>
2024-09-25 18:12:30 -0400

    documentacion anadida en los views y typos corregidos en plantillas

--- Financiera/members/views.py
+++ Financiera/members/views.py
@@ -7,18 +7,40 @@ from .forms import ClienteForm
 
 """ Vistas para el manejo de las operaciones de los bancos  """
 class BancoListView(ListView):
+    """
+        Vista  de tipo ListView para listar los bancos .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+    """
     model = Banco
     template_name = 'bancos/banco_list.html' 
     context_object_name = 'bancos'
 
 
 class BancoDetailView(DetailView):
+    """
+        Vista  de tipo DetailView que muestra el detalle de un banco en especifico .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+    """
     model = Banco
     template_name = 'bancos/banco_detail.html' 
     context_object_name = 'banco'
 
 
 class BancoCreateView(CreateView):
+    """
+        Vista  de tipo CreateView usada para crear los bancos .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+        - success_url: url a la cual se redirige despues de ser ejecutada correctamente
+    """
     model = Banco
     template_name = 'bancos/banco_form.html' 
     fields = ['nombre', 'tipo', 'direccion']
@@ -29,22 +51,38 @@ class BancoCreateView(CreateView):
         return super().form_valid(form)
     
 class BancoUpdateView(UpdateView):
+    """
+        Vista  de tipo CreateView usada para crear los bancos .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+        - success_url: url a la cual se redirige despues de ser ejecutada correctamente
+    """
     model = Banco
     template_name = 'bancos/banco_form.html' 
     fields = ['nombre', 'tipo', 'direccion']
     success_url = reverse_lazy('members:bancos-list') 
+    
     def form_valid(self, form):
-        # Codigo apra mandar mensaje de exito al usuario de la accion realizada
+        """ Codigo apra poder mandar mensaje de exito al usuario de la accion realizada """
         messages.success(self.request, '¡El banco se actualizó exitosamente!')
         return super().form_valid(form)
     
 class BancoDeleteView(DeleteView):
+    """
+        Vista  de tipo DeleteView usada para eliminar un banco .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - template_name: Nombre de la plantilla que se va a  renderizar
+        - success_url: url a la cual se redirige despues de ser ejecutada correctamente
+    """
     model = Banco
     template_name = 'bancos/banco_confirm_delete.html' 
     success_url = reverse_lazy('members:bancos-list')  # Redirige a la lista de bancos después de eliminar el banco actual
 
     def form_valid(self, form):
-        # metodo apra mandar mensaje de elimiancion correcta
+        """ metodo apra mandar mensaje de elimiancion correcta """
         obj = self.get_object()
         messages.success(self.request, f' Banco {obj} eliminado correctamente')
         return super().form_valid(form)
@@ -54,6 +92,14 @@ class BancoDeleteView(DeleteView):
 
 """ Vistas para el modelo  Cliente  """
 class ClienteListView(ListView):
+    """
+        Vista  de tipo ListView para listar los bancos .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+        - success_url: url a la cual se redirige despues de ser ejecutada correctamente
+    """
     model = Cliente
     template_name = 'clientes/cliente_list.html' 
     context_object_name = 'clientes'
@@ -63,12 +109,29 @@ class ClienteListView(ListView):
     
 
 class ClienteDetailView(DetailView):
+    """
+        Vista  de tipo DetailView que muestra el detalle de un cliente en especifico .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+    """
     model = Cliente
     template_name = 'clientes/cliente_detail.html' 
     context_object_name = 'cliente'
 
 
 class ClienteCreateView(CreateView):
+    """
+        Vista  de tipo CreateView usada para crear los bancos .
+        posee parámetros:
+        
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+        - success_url: url a la cual se redirige despues de ser ejecutada correctamente
+        - form_class: formulario utilizado a la hora de crear un cliente, se encuentra definido en forms.py y agrega widgets para el template
+    """
     model = Cliente
     template_name = 'clientes/cliente_form.html' 
     success_url = reverse_lazy('members:cliente-list') 
@@ -76,7 +139,7 @@ class ClienteCreateView(CreateView):
     form_class = ClienteForm
     
     def form_valid(self, form):
-        # Codigo apra mandar mensaje de exito al usuario de la accion realizada
+        """ Codigo apra mandar mensaje de exito al usuario de la accion realizada """
         messages.success(self.request, '¡El cliente se creó exitosamente!')
         return super().form_valid(form)
     
@@ -112,6 +175,15 @@ find e vistas apra el cliente  """
 """ inicio de las vistas para el manejo de creditos  """
 
 class CreditoCreateView(CreateView):
+    """
+        Vista  de tipo CreateView usada para crear los creditos .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - fields : para especificar que campos se van a mostrar en el formulario
+        - template_name: Nombre de la plantilla que se va a  renderizar
+        - success_url: url a la cual se redirige despues de ser ejecutada correctamente
+    """
     model = Credito
     template_name = 'creditos/credito_form.html' 
     fields = '__all__'
@@ -128,12 +200,27 @@ class CreditoCreateView(CreateView):
     
     
 class CreditoListView(ListView):
+    """
+        Vista  de tipo ListView para listar los bancos .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+       
+    """
     model = Credito
     template_name = 'creditos/credito_list.html' 
     context_object_name = 'creditos'
     
     
 class CreditoDetailView(DetailView):
+    """
+        Vista  de tipo DetailView que muestra el detalle de un credito en especifico .
+        posee parámetros:
+        - model: modelo utilizado para crear la class based view 
+        - context_object_name: Nombre del objeto de contexto para la plantilla
+        - template_name: Nombre de la plantilla que se va a  renderizar
+    """
     model = Credito
     template_name = 'creditos/credito_detail.html' 
     context_object_name = 'credito'
--- readme.md
+++ readme.md
@@ -4,8 +4,16 @@
 2.  Posicionarse en la carpeta raiz del proyecto por `cd .\FinancieraG\ `
 3.  Ejecutar el comando `docker-compose up`
 4.  En el docker compose se encuentran listados dos servicios que deberian inicializarse despues de ejecutar el comando `docker-compose up `estos servicios son `web` y `database` , verificar que estan conrriendo con el comando `docker-compose ps`
+
 5.  Ejecutar el comando `docker-compose exec -i -t web bash` para poder ejecutar las migraciones de la basede datos dentro del contenedor del servidor web, esto abrira una consola
-6.  Ejecutar el comando `python manage.py migrate`
+
+![image](https://github.com/user-attachments/assets/be9e803c-d712-4992-96b2-6b251f068d28)  
+6. Ejecutar el comando `python manage.py migrate` ![image](https://github.com/user-attachments/assets/398b4af5-948f-4bc7-919b-161893939fea)
+
 7.  Crear un super usuario para poder ingresar en el admin de django si se desea con `python manage.py createsuper user `
 8.  realizar un `docker-compose down` y luego `docker-compose up` para inicializar el servidor nuevamente
+    ![image](https://github.com/user-attachments/assets/6a1cfabb-2ea6-40d4-aaa1-a1075fbc5fd9)
+
 9.  Ingresar al navegador ` [localhost:8000](http://localhost:8000/)`
+
+![image](https://github.com/user-attachments/assets/55a2d614-e91d-439b-8ccb-25a4b430bac1)
